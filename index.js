942bf195b38c46971fbfbc25ce57f0349833c0e1
ammy <camila.barrientos2020@twk.cl>
2023-05-24 01:56:24 -0300

    agregado los graficos

--- index.js
+++ index.js
@@ -4,10 +4,36 @@ var enviar = document.querySelector('#enviar');
 enviar.addEventListener('click',function(){
     $.ajax({
         type: "get",
-        url: "https://pokeapi.co/api/v2/pokemon/",
+        url: "https://pokeapi.co/api/v2/pokemon/"+nombrePokemon.value,
         dataType: "json",
         success: function (response) {
             console.log(response);
+            graficar(response);
         }
     });
 })
+
+function graficar(response){
+    var chart = new CanvasJS.Chart("chartContainer", {
+        theme: "dark2", // "light2", "dark1", "dark2"
+        animationEnabled: true, // change to true		
+        title:{
+            text: "Pokemon " + response.name
+        },
+        data: [
+        {
+            // Change type to "bar", "area", "spline", "pie",etc.
+            type: "column",
+            dataPoints: [
+                { label: response.stats[0].stat.name,  y: response.stats[0].base_stat },
+                { label: response.stats[1].stat.name, y: response.stats[1].base_stat  },
+                { label: response.stats[2].stat.name, y: response.stats[2].base_stat  },
+                { label: response.stats[3].stat.name,  y: response.stats[3].base_stat },
+                { label: response.stats[4].stat.name, y: response.stats[4].base_stat  }
+            ]
+        }
+        ]
+    });
+    chart.render();
+}
+
